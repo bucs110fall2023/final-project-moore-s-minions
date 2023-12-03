a66a8f84d0fb2be51e4a5b2776ee8b70f44c66a6
SammyHaridy1 <sharidy@binghamton.edu>
2023-12-02 19:06:57 -0500

    added bubble sort

--- src/sample_controller.py
+++ src/sample_controller.py
@@ -32,95 +32,6 @@
 
 #       #redraw
 
-# import pygame
-# import random
-
-# # Initialize Pygame
-# pygame.init()
-
-# # Constants
-# SCREEN_WIDTH = 800
-# SCREEN_HEIGHT = 600
-# RECTANGLE_WIDTH = 50
-# RECTANGLE_HEIGHT_MAX = 500
-# NUM_RECTANGLES = 10
-
-# # Colors
-# BLACK = (0, 0, 0)
-# WHITE = (255, 255, 255)
-# RECTANGLE_COLOR = (0, 0, 255)
-# SWAP_COLOR = (255, 0, 0)
-# FINAL_COLOR = (0, 255, 0)
-# TEXT_COLOR = (255, 255, 255)
-
-# # Function to generate random heights for rectangles
-# def generate_random_heights(num_rectangles):
-#     return [random.randint(50, RECTANGLE_HEIGHT_MAX) for _ in range(num_rectangles)]
-
-# # Bubble Sort algorithm
-# def bubble_sort(heights):
-#     n = len(heights)
-#     for i in range(n):
-#         for j in range(0, n-i-1):
-#             if heights[j] > heights[j+1]:
-#                 # Swap heights
-#                 heights[j], heights[j+1] = heights[j+1], heights[j]
-#                 # Update display
-#                 update_display(j, j+1, heights)
-
-# # Function to update the display
-# def update_display(i, j, heights):
-#     screen.fill(BLACK)
-#     for idx in range(NUM_RECTANGLES):
-#         x_position = idx * (SCREEN_WIDTH // NUM_RECTANGLES)
-#         y_position = SCREEN_HEIGHT - heights[idx]
-
-#         if idx == i or idx == j:
-#             # Highlight the rectangles being swapped
-#             pygame.draw.rect(screen, SWAP_COLOR, (x_position, y_position, RECTANGLE_WIDTH, heights[idx]))
-#         elif x_position == idx * (SCREEN_WIDTH // NUM_RECTANGLES):
-#             # Mark the rectangles at the right side as green
-#             pygame.draw.rect(screen, FINAL_COLOR, (x_position, y_position, RECTANGLE_WIDTH, heights[idx]))
-#         else:
-#             pygame.draw.rect(screen, RECTANGLE_COLOR, (x_position, y_position, RECTANGLE_WIDTH, heights[idx]))
-
-#         text = font.render(str(heights[idx]), True, TEXT_COLOR)
-#         text_rect = text.get_rect(center=(x_position + RECTANGLE_WIDTH // 2, SCREEN_HEIGHT - 10))
-#         screen.blit(text, text_rect)
-
-#     pygame.display.flip()
-#     pygame.time.delay(500)  # Delay to make the sorting steps visible
-
-# # Create a list of random heights for rectangles
-# rectangle_heights = generate_random_heights(NUM_RECTANGLES)
-
-# # Create the screen
-# screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-# pygame.display.set_caption("Sorting Rectangles")
-
-# # Font for displaying text
-# font = pygame.font.Font(None, 24)
-
-# # Clock to control the frame rate
-# clock = pygame.time.Clock()
-
-# # Main loop
-# running = True
-# while running:
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             running = False
-
-#     # Run the Bubble Sort algorithm
-#     bubble_sort(rectangle_heights)
-
-#     # Quit the loop after the sorting is done
-#     running = False
-
-# # Quit Pygame
-# pygame.quit()
-
-
 import pygame
 import random
 
@@ -139,7 +50,6 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RECTANGLE_COLOR = (0, 0, 255)
 SWAP_COLOR = (255, 0, 0)
-FINAL_COLOR = (0, 255, 0)
 TEXT_COLOR = (255, 255, 255)
 
 # Function to generate random heights for rectangles
@@ -147,7 +57,7 @@ def generate_random_heights(num_rectangles):
     return [random.randint(50, RECTANGLE_HEIGHT_MAX) for _ in range(num_rectangles)]
 
 # Bubble Sort algorithm
-def bubble_sort(heights, sorted_indices):
+def bubble_sort(heights):
     n = len(heights)
     for i in range(n):
         for j in range(0, n-i-1):
@@ -155,10 +65,10 @@ def bubble_sort(heights, sorted_indices):
                 # Swap heights
                 heights[j], heights[j+1] = heights[j+1], heights[j]
                 # Update display
-                update_display(j, j+1, heights, sorted_indices)
+                update_display(j, j+1, heights)
 
 # Function to update the display
-def update_display(i, j, heights, sorted_indices):
+def update_display(i, j, heights):
     screen.fill(BLACK)
     for idx in range(NUM_RECTANGLES):
         x_position = idx * (SCREEN_WIDTH // NUM_RECTANGLES)
@@ -167,11 +77,7 @@ def update_display(i, j, heights, sorted_indices):
         if idx == i or idx == j:
             # Highlight the rectangles being swapped
             pygame.draw.rect(screen, SWAP_COLOR, (x_position, y_position, RECTANGLE_WIDTH, heights[idx]))
-        elif idx in sorted_indices:
-            # Mark the rectangles in their final sorted positions as green
-            pygame.draw.rect(screen, FINAL_COLOR, (x_position, y_position, RECTANGLE_WIDTH, heights[idx]))
         else:
-            # Regular blue color for other rectangles
             pygame.draw.rect(screen, RECTANGLE_COLOR, (x_position, y_position, RECTANGLE_WIDTH, heights[idx]))
 
         text = font.render(str(heights[idx]), True, TEXT_COLOR)
@@ -196,26 +102,17 @@ clock = pygame.time.Clock()
 
 # Main loop
 running = True
-sorted_indices = set()  # Track indices that are in their final sorted positions
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     # Run the Bubble Sort algorithm
-    bubble_sort(rectangle_heights, sorted_indices)
-
-    # Check for indices in their final sorted positions
-    for idx in range(NUM_RECTANGLES):
-        if idx not in sorted_indices and rectangle_heights[idx] == SCREEN_HEIGHT - idx * (RECTANGLE_HEIGHT_MAX // NUM_RECTANGLES):
-            sorted_indices.add(idx)
+    bubble_sort(rectangle_heights)
 
     # Quit the loop after the sorting is done
-    if len(sorted_indices) == NUM_RECTANGLES:
-        running = False
+    running = False
 
 # Quit Pygame
 pygame.quit()
-
-
 
